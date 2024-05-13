4ceed8a0867b288080294de6f5e2540534cab001
Talha Sattar <160661345+talhasattar06@users.noreply.github.com>
2024-05-13 05:01:51 +0500

    pushed code

---
+++ index.ts
@@ -0,0 +1,48 @@
+#! /usr/bin/env node
+
+import inquirer from "inquirer";
+import chalk from "chalk";
+
+const currency: any = {
+    USD: 1,              // US Dollar, Base Currency
+    CHF: 0.90,           // Swiss Franc
+    EUR: 0.92,           // Euro
+    GBP: 0.79,           // British Pound
+    CAD: 1.36,           // Canadian Dollar
+    AUD: 1.51,           // Australian Dollar
+    NZD: 1.66,           // New Zealand Dollar
+    QAR: 3.64,           // Qatari Riyal
+    SAR: 3.75,           // Saudi Arabian Riyal
+    JPY: 155,            // Japanese Yen
+    INR: 83.54,          // Indian Rupee
+    PKR: 278.14          // Pakistani Rupee
+};
+
+let answer: any = await inquirer.prompt([
+    {
+        name: "from",
+        message: chalk.hex('#7DF9FF')("Select From Currency"),
+        type: "list",
+        choices: ["USD", "CHF", "EUR", "GBP", "CAD", "AUD", "NZD", "QAR", "SAR", "JPY", "INR", "PKR"]
+    },
+    {
+        name: "to",
+        message: chalk.hex('#7DF9FF')("Select To Currency"),
+        type: "list",
+        choices: ["USD", "CHF", "EUR", "GBP", "CAD", "AUD", "NZD", "QAR", "SAR", "JPY", "INR", "PKR"]
+    },
+    {
+        name: "amount",
+        message: chalk.hex('#FFFD37')("Enter Your Amount:"),
+        type: "number"
+    }
+]);
+
+let fromAmount = currency[answer.from];
+let toAmount = currency[answer.to];
+let amount = answer.amount;
+let baseAmount = amount / fromAmount;
+let convertAmount = baseAmount * toAmount;
+let finalAmount = convertAmount.toFixed(2);
+
+console.log(chalk.hex('#FF00FF')(`${amount} ${answer.from} in ${answer.to} equals to ${finalAmount} ${answer.to}`));
